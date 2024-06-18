98a191e9d136deac32a3e89964875316ff71b4cc
Curtis Crouse <cecrouse96@yahoo.com>
2024-06-18 07:49:41 +0000

    Created the job details page for the frontend.

--- app.py
+++ app.py
@@ -1,18 +1,25 @@
 from flask import Flask, jsonify, render_template
 
-from database import load_jobs_from_db
+from database import load_job_from_db, load_jobs_from_db
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     jobs = load_jobs_from_db()
-    return render_template("home.html", jobs=jobs, company_name="Devcentral")
+    return render_template("home.html", jobs=jobs)
 
 @app.route("/jobs")
 def list_jobs():
     jobs = load_jobs_from_db()
     return jsonify(jobs)
+
+@app.route("/job/<id>") 
+def show_job(id):
+    job = load_job_from_db(id)
+    if not job:
+        return "Not Found", 404
+    return render_template("jobpage.html", job=job)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
